c60842c97cde5d46b922ef85c2eb05950a678125
Your Name <muhammad04rahim03s@gmail.com>
2024-12-30 02:08:55 -0800

    NextJs UI/UX Hackathon: e-comerce website

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
+import Footer from "./components/footer";
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -27,8 +29,10 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+       
       >
         {children}
+        <Footer/>
       </body>
     </html>
   );
